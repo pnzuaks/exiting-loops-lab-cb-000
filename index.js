d31921a0cf24bfdbe7b27d23903fa587de1c827c
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-18 21:24:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function breakOut(array, changeValue, stopValue) {
     if (array[i] === stopValue) {
       break
     }
+    array[i] = changeValue
 
     {
-      array[i] = changeValue
       return array
     }
   }
